d6ea57995f22f9028b305e7bb9a0e634cca1c0ab
abdi mohamed <aabdymohamed@gmail.com>
2022-03-16 23:43:14 +0100

    commit

---
+++ src/components/form.jsx
@@ -0,0 +1,37 @@
+import React,{useState} from 'react'
+import Login from './login'
+
+
+function Form() {
+    const [name,setName]=useState("")
+    const [passsword, setPassword]=useState("")
+
+
+    const handleSubmit = ((event)=>{
+        event.preventDefault()
+        console.log("submitted muthafucker")
+        
+    })
+    return (
+        <div>
+          
+
+           <h1>Register</h1>
+                <form>
+                    <input type="text" placeholder="name" value={name} onChange={(e)=>setName(e.target.value)}/>
+                    <input type="password" placeholder="password" value={passsword} onChange={(e)=>setPassword(e.target.value)}/>
+                    
+                </form>
+                    <p>{name}</p>
+                    <p>{passsword}</p>
+                <button onClick={handleSubmit} >create</button>
+                <Login
+                name = {name} 
+                passsword= {passsword}
+            />
+
+        </div>
+    )
+}
+
+export default Form
---
+++ src/components/login.js
@@ -0,0 +1,37 @@
+import  React ,{useState} from 'react'
+// import Form from "./form"
+function Login(props) {
+   const [newname, setNewName] = useState("")
+   const [newpassword, setNewPassword] = useState("")
+
+  
+   
+   
+   let handleLogin = (event)=>{
+    event.preventDefault()
+    if (props.passsword === newpassword || props.name === newname){
+        console.log("welcome muthafuck")
+    }
+    else{
+        console.log("nope")
+    }
+}
+    return (
+        <div>
+             <h1>Login</h1>
+                <form>
+                    <input type="text" placeholder="name" value= {newname}onChange={(e)=>setNewName(e.target.value)}/>
+                    <input type="password"  placeholder="password" value={newpassword} onChange={(e)=>setNewPassword(e.target.value)} />
+                    <button onClick={handleLogin} >Login</button>
+
+                <p>{console.log(newname)}</p>
+                <p>{props.name}</p>
+
+                </form>
+                  
+               
+        </div>
+    )
+}
+
+export default Login
